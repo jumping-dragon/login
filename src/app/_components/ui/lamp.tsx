d0f4508a419ae74a4bacf58180981a0f43103616
Alvin Januar <dev@alvinjanuar.com>
2025-06-29 10:25:11 +0800

    fix: lamp component for tailwind v4

--- src/app/_components/ui/lamp.tsx
+++ src/app/_components/ui/lamp.tsx
@@ -15,10 +15,7 @@ export const Lamp = () => {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        style={{
-          backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-        }}
-        className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-blue-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+        className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-[conic-gradient(from_70deg_at_center_top,theme(colors.blue.500),transparent,transparent)] text-white"
       >
         <div className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
         <div className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
@@ -31,10 +28,7 @@ export const Lamp = () => {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        style={{
-          backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-        }}
-        className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-blue-500 text-white [--conic-position:from_290deg_at_center_top]"
+        className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-[conic-gradient(from_290deg_at_center_top,transparent,transparent,theme(colors.blue.500))] text-white"
       >
         <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
         <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
@@ -81,14 +75,14 @@ export const LampContainer = ({
     <div
       className={cn(
         "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full rounded-md z-0",
-        className,
+        className
       )}
     >
       <Lamp />
       <div
         className={cn(
           "relative z-50 flex -translate-y-80 flex-col items-center px-5",
-          childrenClassName,
+          childrenClassName
         )}
       >
         {children}
